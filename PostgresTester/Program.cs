12bf7b5c1d909dcfc92fd37010bf5d5cb8c5561b
Luke Briner <lukiebriner@gmail.com>
2023-06-10 15:41:57 +0100

    Add session attributes to connection strings

--- PostgresTester/Program.cs
+++ PostgresTester/Program.cs
@@ -14,13 +14,13 @@ namespace PostgresTester
             builder.Services.AddDbContext<GeneralContext>(options =>
             {
                 //options.UseNpgsql("host=192.168.56.211;port=5000;database=books;user id=books;password=bookspassword;timeout=3;");    // Proxy
-                //options.UseNpgsql("host=192.168.56.201,192.168.56.202,192.168.56.203;port=5432;database=books;user id=books;password=bookspassword;timeout=3;");      // Using multiple hosts
+                //options.UseNpgsql("host=192.168.56.201,192.168.56.202,192.168.56.203;port=5432;database=books;user id=books;password=bookspassword;timeout=3;Target Session Attributes=primary");      // Using multiple hosts
                 options.UseNpgsql("host=192.168.56.201;port=5432;database=books;user id=books;password=bookspassword;timeout=3;");      // Direct to postgres1
             });
             builder.Services.AddDbContext<ReadonlyContext>(options =>
             {
                 //options.UseNpgsql("host=192.168.56.211;port=5001;database=books;user id=books;password=bookspassword;timeout=3;");    // Proxy
-                //options.UseNpgsql("host=192.168.56.202,192.168.56.203,192.168.56.201;port=5432;database=books;user id=books;password=bookspassword;timeout=3;");      // Using multiple hosts
+                //options.UseNpgsql("host=192.168.56.202,192.168.56.203,192.168.56.201;port=5432;database=books;user id=books;password=bookspassword;timeout=3;Target Session Attributes=prefer-standby");      // Using multiple hosts
                 options.UseNpgsql("host=192.168.56.201;port=5432;database=books;user id=books;password=bookspassword;timeout=3;");      // Direct to postgres1
             });
             builder.Services.AddDbContext<Postgres1Context>(options =>
